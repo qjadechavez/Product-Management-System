7ca74866df0bb57f781ce325e360ead432d03272
Jhon Arol De Chavez <qjadechavez@tip.edu.ph>
2023-06-16 10:44:57 +0800

    First commit

---
+++ src/productmanagementsystem/ProductManagementSystem.java
@@ -0,0 +1,17 @@
+
+package productmanagementsystem;
+
+public class ProductManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginScreen LoginFrame = new LoginScreen();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null);
+    }
+    
+}
